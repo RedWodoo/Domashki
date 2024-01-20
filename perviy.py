b6bc6715ea18083713e18963944e23ea655899ba
Chma4ka <alikakenov567@gmail.com>
2024-01-20 15:49:11 +0600

    domashka 19

--- perviy.py
+++ perviy.py
@@ -1,9 +1,14 @@
-
+'''
+program
+'''
 try:
-    qwe = 2
+    QWE = 2
     number = int(input('Введите число: '))
     def plus_two(a,num):
+        '''
+        program
+        '''
         print(f'Ответ: {a + num}')
-    plus_two(qwe, number)
+    plus_two(QWE, number)
 except ValueError:
     print("Ожидаемый тип данных— число!")
